4a1bbe7e982bfc6266533303b0fa739477d2df52
Tomaz Ferreira De Lima <toferrei@c1r11s1.42lisboa.com>
2024-04-09 17:51:21 +0100

    vamos dar um nome mais decente

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,7 +6,7 @@
 /*   By: toferrei <toferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 15:06:05 by toferrei          #+#    #+#             */
-/*   Updated: 2024/04/09 15:42:44 by toferrei         ###   ########.fr       */
+/*   Updated: 2024/04/09 17:33:59 by toferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,6 @@
 
 int	ft_isalpha(int c)
 {
-	int	i;
-
-	i = 0;
 	if (('A' <= c && c <= 'Z')
 		|| ('a' <= c && c <= 'z'))
 		return (1);
