2677f6c48316e60451aa4467e42db7a64fe04d54
Tomaz Ferreira De Lima <toferrei@c1r11s1.42lisboa.com>
2024-04-10 12:14:52 +0100

    memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,30 +6,50 @@
 /*   By: toferrei <toferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 16:12:36 by toferrei          #+#    #+#             */
-/*   Updated: 2024/04/09 15:57:52 by toferrei         ###   ########.fr       */
+/*   Updated: 2024/04/10 12:14:01 by toferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include <stdio.h>
+#include <string.h>
 
-void	*ft_memset(char *s, int c, size_t n)
+void	*ft_memset(void *s, int c, size_t n)
 {
 	unsigned long	i;
 
 	i = 0;
-	while (s[i] && i < n)
+	while (((int *)s)[i] && i < n)
 	{
-		((char *)s)[i] = c;
+		((int *)s)[i] = c;
 		i++;
 	}
 	return (s);
 }
 
-int main (void)
+/*int main (void)
 {
-	char * s = "Nao sei++";
-	printf("%p", s);
+	int i = 0;
 	
-	printf("%p", ft_memset(s, 's', sizeof (char) * 10));
-}
+	int s[10]= {0, 1, 2 , 3, 4, 5, 6, 7, 8, 9};
+	int *res1 = memset(s, 1, 4);
+	int *res2 = ft_memset(s, 1, 4);
+	while (i < 10)
+	{
+		printf("%d", s[i]);
+		i++;
+	}
+	i = 0;
+	printf("\n");
+	while (i < 10)
+	{
+		printf("%d", res1[i]);
+		i++;
+	}
+	i = 0;
+	printf("\n");
+	while (i < 10)
+	{
+		printf("%d", res2[i]);
+		i++;
+	}
+} */
