046da932b52c7559a2ba11338640005b9ab3b8e4
Tomaz <tomazf4@gmail.com>
2024-04-12 22:18:03 +0100

    ft_memcmp outside_42

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: toferrei <toferrei@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: etom <etom@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 19:22:09 by toferrei          #+#    #+#             */
-/*   Updated: 2024/04/12 19:54:05 by toferrei         ###   ########.fr       */
+/*   Updated: 2024/04/12 22:15:26 by etom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,25 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	
-	while (n - 1 > 0 && (unsigned char *)s1 == (unsigned char *)s2)
-	{
-		s1++;
-		s2++;
-		n--;
-	}
-	if (n == 0 || s1 - s2 == 0)
+	size_t			i;
+	unsigned char	*ss1;
+	unsigned char	*ss2;
+
+	ss1 = (unsigned char *)s1;
+	ss2 = (unsigned char *)s2;
+	i = 0;
+	while (n - 1 > i && ss1[i] == ss2[i])
+		i++;
+	if (n == 0 || ss1[i] == ss2[i])
 		return (0);
-	if ((unsigned char *)s1 < (unsigned char *)s2)
+	if (ss1[i] > ss2[i])
 		return (1);
 	else
 		return (-1);
 }
 
-int main (void)
+/*int	main (void)
 {
-	
 	printf("%d\n", ft_memcmp("salut", "salut", 5));
 	printf("%d\n\n",  memcmp("salut", "salut", 5));
 	printf("%d\n", ft_memcmp("t\200", "t\0", 2));
@@ -41,13 +42,13 @@ int main (void)
 	printf("%d\n", ft_memcmp("test", "tEst", 4));
 	printf("%d\n\n",  memcmp("test", "tEst", 4));
 	printf("%d\n", ft_memcmp("", "test", 4));
-	printf("%d\n\n",  memcmp("", "test", 4));
+	printf("%d\n\n", memcmp("", "test", 4));
 	printf("%d\n", ft_memcmp("test", "", 4));
-	printf("%d\n\n",  memcmp("test", "", 4));
+	printf("%d\n\n", memcmp("test", "", 4));
 	printf("%d\n", ft_memcmp("abcdefghij", "abcdefgxyz", 7));
 	printf("%d\n\n",  memcmp("abcdefghij", "abcdefgxyz", 7));
 	printf("%d\n", ft_memcmp("abcdefgh", "abcdwxyz", 6));
 	printf("%d\n\n",  memcmp("abcdefgh", "abcdwxyz", 6));
 	printf("%d\n", ft_memcmp("zyxbcdefgh", "abcdefgxyz", 0));
 	printf("%d\n",    memcmp("zyxbcdefgh", "abcdefgxyz", 0));
-}
+}*/
