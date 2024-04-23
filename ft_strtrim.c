2a1818444014b7ba84106206d6fae2f80e4b9a04
Tomaz Ferreira De Lima <toferrei@c1r11s1.42lisboa.com>
2024-04-23 13:27:38 +0100

    Strtrim fn + norm

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,30 +3,70 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: etom <etom@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: toferrei <toferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 00:11:56 by etom              #+#    #+#             */
-/*   Updated: 2024/04/22 00:33:20 by etom             ###   ########.fr       */
+/*   Updated: 2024/04/23 13:26:52 by toferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	chrchk(char c, char const *set)
+{
+	int	n;
+
+	n = 0;
+	while (set[n])
+	{
+		if (set[n] == c)
+			return (1);
+		n++;
+	}
+	return (0);
+}
+
+char	*nstr(char const *s1, int d, int e, int k)
+{
+	char	*cpy;
+	int		c;
+
+	cpy = malloc(sizeof * cpy * (k - (d + e) + 1));
+	if (!cpy)
+		return (0);
+	c = 0;
+	while (c < k - (d + e))
+	{
+		cpy[c] = s1[d + c];
+		c++;
+	}
+	cpy[c] = '\0';
+	return (cpy);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	int	c;
-	int d;
+	int		c;
+	int		d;
+	int		e;
+	int		k;
 
 	c = 0;
-	if (s1[0] == set[0])
-		while (s1[c] == set[c])
-			c++;
-	if (set[c] == '\0')
+	d = 0;
+	e = 0;
+	k = ft_strlen(s1);
+	if (!s1)
+		return ((char *)s1);
+	while (chrchk(s1[c], set) == 1)
+	{
+		c++;
 		d++;
+	}
 	c = ft_strlen(s1) - 1;
-	if (s1[c] == set[0])
-		while (s1[c] == set[c])
-			c++;
-	if (set[c] == '\0')
-		d++;
+	while (chrchk(s1[c], set) == 1)
+	{
+		c--;
+		e++;
+	}
+	return (nstr(s1, d, e, k));
 }
